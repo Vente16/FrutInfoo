2d740cfd0f7bfd52808976062b1a869da0270fc3
oscar <oe6@misena.edu.co>
2017-04-18 20:50:46 -0500

    arrelgo

--- build/generated/src/org/apache/jsp/Mod_002dRecursosHumanos/RegistrarEmpleados_jsp.java
+++ build/generated/src/org/apache/jsp/Mod_002dRecursosHumanos/RegistrarEmpleados_jsp.java
@@ -14,7 +14,7 @@ public final class RegistrarEmpleados_jsp extends org.apache.jasper.runtime.Http
   static {
     _jspx_dependants = new java.util.ArrayList<String>(2);
     _jspx_dependants.add("/Mod-RecursosHumanos/../headerModulos.jspf");
-    _jspx_dependants.add("/Mod-RecursosHumanos/../footer.jspf");
+    _jspx_dependants.add("/Mod-RecursosHumanos/../footerModulos.jspf");
   }
 
   private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
@@ -68,7 +68,7 @@ public final class RegistrarEmpleados_jsp extends org.apache.jasper.runtime.Http
       out.write("\r\n");
       out.write("\r\n");
       out.write("<header>\r\n");
-      out.write("    <div class=\"container\">\r\n");
+      out.write("   \r\n");
       out.write("        <nav class=\"navbar navbar-default\" role=\"navigation\">\r\n");
       out.write("            <!-- El logotipo y el icono que despliega el menú se agrupan\r\n");
       out.write("                 para mostrarlos mejor en los dispositivos móviles -->\r\n");
@@ -80,7 +80,7 @@ public final class RegistrarEmpleados_jsp extends org.apache.jasper.runtime.Http
       out.write("                    <span class=\"icon-bar\"></span>\r\n");
       out.write("                    <span class=\"icon-bar\"></span>\r\n");
       out.write("                </button>\r\n");
-      out.write("                <img src=\"../img/logolj.png\" width=\"50px\" style=\"vertical-align: middle; float: left;\" alt=\"\">\r\n");
+      out.write("                <img src=\"../img/logo.png\" width=\"50px\" style=\"vertical-align: middle; float: left;\" alt=\"\">\r\n");
       out.write("                <a class=\"navbar-brand\" href=\"#\">LA JUGOSA</a>\r\n");
       out.write("            </div>\r\n");
       out.write("\r\n");
@@ -90,10 +90,10 @@ public final class RegistrarEmpleados_jsp extends org.apache.jasper.runtime.Http
       out.write("              <ul class=\"nav navbar-nav navbar-right\">\r\n");
       out.write("      <li class=\"dropdown\"><a class=\"dropdown-toggle\" data-toggle=\"dropdown\" href=\"#\"><span class=\"glyphicon glyphicon-briefcase\"></span>Administrativo<span class=\"caret\"></span></a>\r\n");
       out.write("        <ul class=\"dropdown-menu\">\r\n");
-      out.write("          <li><a href=\"MOD-ADMINISTRATIVO/Administrativo_Autorizaciones.html\">Autorizaciones</a></li>\r\n");
-      out.write("          <li><a href=\"MOD-ADMINISTRATIVO/Administrativo_Ventas.html\">Informe de ventas</a></li>\r\n");
-      out.write("          <li><a href=\"MOD-ADMINISTRATIVO/Administrativo_Insumos.html\">Informe de Insumos</a></li>\r\n");
-      out.write("          <li><a href=\"MOD-ADMINISTRATIVO/Administatrivo_InformeEmpleados.html\">Informe de empleados</a></li>\r\n");
+      out.write("          <li><a href=\"../Mod-Administrativo/Autorizaciones_admin.jsp\">Autorizaciones</a></li>\r\n");
+      out.write("          <li><a href=\"../Mod-Administrativo/Ventas_admin.jsp\">Informe de ventas</a></li>\r\n");
+      out.write("          <li><a href=\"../Mod-Administrativo/Insumos_admin.jsp\">Informe de Insumos</a></li>\r\n");
+      out.write("          <li><a href=\"../Mod-Administrativo/InformeEmpleados_admin.jsp\">Informe de empleados</a></li>\r\n");
       out.write("        </ul>\r\n");
       out.write("      </li>\r\n");
       out.write("\r\n");
@@ -129,7 +129,7 @@ public final class RegistrarEmpleados_jsp extends org.apache.jasper.runtime.Http
       out.write("      \r\n");
       out.write("         <li class=\"dropdown\"><a class=\"dropdown-toggle\" data-toggle=\"dropdown\" href=\"#\">Clientes<span class=\"caret\"></span></a>\r\n");
       out.write("        <ul class=\"dropdown-menu\">\r\n");
-      out.write("          <li><a href=\"#\">Registrar clientes</a></li>\r\n");
+      out.write("            <li><a href=\"../Mod-Clientes/RegisCliente.jsp\">Registrar clientes</a></li>\r\n");
       out.write("          <li><a href=\"#\">Listar clientes</a></li>\r\n");
       out.write("          <li><a href=\"#\">Solicitar domicilio</a></li>\r\n");
       out.write("        </ul>\r\n");
@@ -148,7 +148,7 @@ public final class RegistrarEmpleados_jsp extends org.apache.jasper.runtime.Http
       out.write("\r\n");
       out.write("            </div>\r\n");
       out.write("        </nav>\r\n");
-      out.write("    </div>\r\n");
+      out.write(" \r\n");
       out.write("</header>\r\n");
       out.write("\r\n");
       out.write("\r\n");
@@ -177,7 +177,9 @@ public final class RegistrarEmpleados_jsp extends org.apache.jasper.runtime.Http
       out.write("\r\n");
       out.write("                    <label class=\"form-control-label\" for=\"index-form1-1-name\"><span class=\"titulo\">Documento de identificación</span></label><br>\r\n");
       out.write("                    <input type=\"text\" class=\"form-control\" name=\"name\"required=\"\" data-form-field=\"Name\" id=\"index-form1-1-name\" placeholder=\"Ingrese el número de documento de identificación\"><br>\r\n");
-      out.write("\r\n");
+      out.write("                    \r\n");
+      out.write("                    <label class=\"form-control-label\" for=\"index-form1-1-name\"><span class=\"titulo\">Email</span></label>\r\n");
+      out.write("                    <input type=\"text\" class=\"form-control\" name=\"name\"required=\"\" data-form-field=\"Name\" id=\"index-form1-1-name\" placeholder=\"Ingrese el cargo del empleado\"><br> \r\n");
       out.write("                    <div class=\"form-group\">\r\n");
       out.write("                        <label class=\"form-control-label\" for=\"index-form-4-4name\"><span class=\"titulo\">Seleccione el tipo de documento</span></label>\r\n");
       out.write("                        <select class=\"form-control\">\r\n");
@@ -216,6 +218,20 @@ public final class RegistrarEmpleados_jsp extends org.apache.jasper.runtime.Http
       out.write("                    </div>\r\n");
       out.write("                    <label class=\"form-control-label\" for=\"index-form1-1-name\"><span class=\"titulo\">Cargo</span></label>\r\n");
       out.write("                    <input type=\"text\" class=\"form-control\" name=\"name\"required=\"\" data-form-field=\"Name\" id=\"index-form1-1-name\" placeholder=\"Ingrese el cargo del empleado\"><br> \r\n");
+      out.write("                    <label class=\"form-control-label\" for=\"index-form1-1-name\"><span class=\"titulo\">Disponibilidad</span></label>\r\n");
+      out.write("                    <input type=\"text\" class=\"form-control\" name=\"name\"required=\"\" data-form-field=\"Name\" id=\"index-form1-1-name\" placeholder=\"Ingrese el cargo del empleado\"><br> \r\n");
+      out.write("                    <label class=\"form-control-label\" for=\"index-form1-1-name\"><span class=\"titulo\">Hora de inicio</span></label>\r\n");
+      out.write("                    <input type=\"text\" class=\"form-control\" name=\"name\"required=\"\" data-form-field=\"Name\" id=\"index-form1-1-name\" placeholder=\"Ingrese el cargo del empleado\"><br> \r\n");
+      out.write("                    <label class=\"form-control-label\" for=\"index-form1-1-name\"><span class=\"titulo\">Hora de salida</span></label>\r\n");
+      out.write("                    <input type=\"text\" class=\"form-control\" name=\"name\"required=\"\" data-form-field=\"Name\" id=\"index-form1-1-name\" placeholder=\"Ingrese el cargo del empleado\"><br> \r\n");
+      out.write("                    <label class=\"form-control-label\" for=\"index-form1-1-name\"><span class=\"titulo\">Inicio contrato</span></label>\r\n");
+      out.write("                    <input type=\"text\" class=\"form-control\" name=\"name\"required=\"\" data-form-field=\"Name\" id=\"index-form1-1-name\" placeholder=\"Ingrese el cargo del empleado\"><br> \r\n");
+      out.write("                    <label class=\"form-control-label\" for=\"index-form1-1-name\"><span class=\"titulo\">Fin contrato</span></label>\r\n");
+      out.write("                    <input type=\"text\" class=\"form-control\" name=\"name\"required=\"\" data-form-field=\"Name\" id=\"index-form1-1-name\" placeholder=\"Ingrese el cargo del empleado\"><br> \r\n");
+      out.write("                    <label class=\"form-control-label\" for=\"index-form1-1-name\"><span class=\"titulo\">Fecha de nacimiento</span></label>\r\n");
+      out.write("                    <input type=\"text\" class=\"form-control\" name=\"name\"required=\"\" data-form-field=\"Name\" id=\"index-form1-1-name\" placeholder=\"Ingrese el cargo del empleado\"><br> \r\n");
+      out.write("                    \r\n");
+      out.write("                    \r\n");
       out.write("                    <button type=\"button\" class=\"btn btn-success\" data-toggle=\"modal\" data-target=\"#Confirmar\">Registrar</button>\r\n");
       out.write("\r\n");
       out.write("            </form>\r\n");
@@ -273,12 +289,10 @@ public final class RegistrarEmpleados_jsp extends org.apache.jasper.runtime.Http
       out.write("    </div>\r\n");
       out.write("\r\n");
       out.write("\r\n");
-      out.write("    ");
-      out.write("\r\n");
-      out.write("\r\n");
+      out.write(" ");
       out.write("\r\n");
       out.write("<footer>\r\n");
-      out.write("    <div class=\"container navbar-fixed-bottom\">\r\n");
+      out.write("    <div class=\"navbar-fixed-bottom\">\r\n");
       out.write("        <div class=\" text-center pie\">\r\n");
       out.write("            <div class=\"row\">\r\n");
       out.write("                <div class=\"col-xs-12\">\r\n");
